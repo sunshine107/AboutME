655023938ea6963b697dbb2f1bd0963b8987b09c
System Administrator <root@whitegolds-Mac-mini.local>
2018-06-02 02:33:32 +0300

    sdf

--- api.js
+++ api.js
@@ -46,12 +46,12 @@ router.get('/aboutMe', function(req, res, next){
 
 var col_menuItems = db.collection('mycontact');
 // Get Todos
-router.get('/aboutMe', function(req, res, next){
+router.get('/mycontact', function(req, res, next){
     db.mycontact.findOne(function(err, mycontact){
         if(err){
            res.send(err);
         } else {
-           res.json(aboutMe);
+           res.json(mycontact);
         }
     });
 });
